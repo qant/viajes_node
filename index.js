58c52999a6ee28e2c848c63e906098d3681a6525
Anton Semenov <anseme@gmail.com>
2022-06-01 09:03:37 +0200

    add simple routing using app.get

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ const app = express();
 //set port or default port
 const port = process.env.PORT || 4000;
 
+app.get('/',(req,res)=>{
+    console.log(`listening GET /`);        
+    res.send('Text Bla bla');
+    //res.json({success: true, id: 1234});
+    
+});
+
+app.get('/home',(req,res)=>{
+    res.send('Home');
+});
+
+app.get('/about',(req,res)=>{
+    res.send('about');
+});
+
+app.get('/contact',(req,res)=>{
+    res.send('contact');
+});
+
+
 //create express listener pass port and callback function
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`);
