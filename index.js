ed152871bccb722449debbbec2392045fbbec4c3
Anton Semenov <anseme@gmail.com>
2022-06-02 09:26:38 +0200

    Working on About section, imgs, icons, etc

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use((req, res, next)=>{
     //console.log(res);
 });
 //set port or default port
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8000;
 
 app.use('/',router);
 
